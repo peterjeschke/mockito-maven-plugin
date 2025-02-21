00f0802f3e99007466e4358a23b5f398d0880f44
Peter Jeschke <peter@jeschke.dev>
2025-02-21 19:44:55 +0100

    Initial commit

---
+++ src/main/java/dev/jeschke/maven/mockito/PrepareAgentMojo.java
@@ -0,0 +1,167 @@
+package dev.jeschke.maven.mockito;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.Optional;
+import java.util.Properties;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static java.util.Objects.requireNonNull;
+import static org.apache.maven.plugins.annotations.LifecyclePhase.INITIALIZE;
+import static org.apache.maven.plugins.annotations.ResolutionScope.TEST;
+
+/**
+ * Prepares the Mockito agent by adding it to the command line of test plugins.
+ * <p>
+ * If the defaults are not overridden, the dependency "org.mockito:mockito-core" has to be in the list of dependencies.
+ * If the dependency is not available, the build will fail. This behavior can be changed by setting failSilent.
+ * <p>
+ * Supports the following test plugins:
+ * <p>
+ * <ul>
+ * <li> maven-surefire-plugin (default)
+ * <li> tycho-surefire-plugin
+ */
+@Execute(phase = INITIALIZE, goal = "prepareAgent")
+@Mojo(name = "prepareAgent", requiresDependencyResolution = TEST, defaultPhase = INITIALIZE, threadSafe = true)
+public class PrepareAgentMojo extends AbstractMojo {
+
+    private static final String TYCHO_SUREFIRE_GROUP_ID = "org.eclipse.tycho";
+    private static final String TYCHO_SUREFIRE_ARTIFACT_ID = "tycho-surefire-plugin";
+    private static final String TYCHO_ARGLINE_PROPERTY = "tycho.testArgLine";
+    private static final String SUREFIRE_ARGLINE_PROPERTY = "argLine";
+
+    private final MavenProject project;
+    private String propertyName = null;
+    private String agentGroupId = "org.mockito";
+    private String agentArtifactId = "mockito-core";
+    private Boolean skipPrepare = null;
+    private boolean skipTests = false;
+    private boolean failSilent = false;
+
+    @Inject
+    public PrepareAgentMojo(final MavenProject project) {
+        this.project = requireNonNull(project);
+    }
+
+    @Override
+    public void execute() throws MojoFailureException {
+        if (skipTests && skipPrepare != FALSE) {
+            getLog().info("Tests are skipped");
+            return;
+        }
+        if (skipPrepare == TRUE) {
+            getLog().info("Goal is skipped");
+            return;
+        }
+        if (isNullOrEmpty(agentGroupId) || isNullOrEmpty(agentArtifactId)) {
+            throw new MojoFailureException("Both agentGroupId and agentArtifactId must be set");
+        }
+        final String actualPropertyName = getPropertyName();
+        final Properties properties = project.getProperties();
+        final String existingArgLine = properties.getProperty(actualPropertyName, "");
+        final Optional<String> artifactPath = buildArtifactPath();
+        if (!artifactPath.isPresent()) {
+            final String artifact = String.format("%s:%s", agentGroupId, agentArtifactId);
+            if (failSilent) {
+                getLog().info(String.format("Could not resolve artifact %s, skipping step", artifact));
+                return;
+            }
+            throw new MojoFailureException(String.format("Could not resolve artifact %s", artifact));
+        }
+        final String mockitoArguments = "-javaagent:" + artifactPath.get();
+        final String newArgline = mockitoArguments + " " + existingArgLine;
+        properties.setProperty(actualPropertyName, newArgline);
+        getLog().info(String.format("%s set to %s", actualPropertyName, newArgline));
+    }
+
+    private String getPropertyName() {
+        if (propertyName != null) {
+            return propertyName;
+        }
+        if (project.getBuildPlugins().stream().anyMatch(this::isTychoTestPlugin)) {
+            return TYCHO_ARGLINE_PROPERTY;
+        }
+        return SUREFIRE_ARGLINE_PROPERTY;
+    }
+
+    private Optional<String> buildArtifactPath() {
+        return project.getArtifacts().stream()
+                .filter(this::isAgentArtifact)
+                .findFirst()
+                .map(Artifact::getFile)
+                .map(File::getAbsolutePath);
+    }
+
+    private boolean isTychoTestPlugin(final Plugin plugin) {
+        return TYCHO_SUREFIRE_GROUP_ID.equals(plugin.getGroupId())
+                && TYCHO_SUREFIRE_ARTIFACT_ID.equals(plugin.getArtifactId());
+    }
+
+    private boolean isAgentArtifact(final Artifact artifact) {
+        return agentGroupId.equals(artifact.getGroupId())
+                && agentArtifactId.equals(artifact.getArtifactId());
+    }
+
+    /**
+     * The maven property to which the command line arguments should be added. If not set, the value will be determined
+     * based on the presence of a test plugin.
+     * <p>
+     * Supported test plugins: maven-surefire-plugin (default), tycho-surefire-plugin
+     */
+    @Parameter(name = "propertyName", property = "mockito.agentPropertyName")
+    public void setPropertyName(final String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    /**
+     * The groupId of the dependency that contains the agent.
+     */
+    @Parameter(name = "agentGroupId", property = "mockito.agentGroupId", defaultValue = "org.mockito")
+    public void setAgentGroupId(final String agentGroupId) {
+        this.agentGroupId = agentGroupId;
+    }
+
+    /**
+     * The artifactId of the dependency that contains the agent.
+     */
+    @Parameter(name = "agentArtifactId", property = "mockito.agentArtifactId", defaultValue = "mockito-core")
+    public void setAgentArtifactId(final String agentArtifactId) {
+        this.agentArtifactId = agentArtifactId;
+    }
+
+    /**
+     * Whether to skip this goal. If set to false, the goal will run even if tests are disabled.
+     */
+    @Parameter(name = "skip", property = "mockito.skipPrepare")
+    public void setSkipPrepare(final Boolean skipPrepare) {
+        this.skipPrepare = skipPrepare;
+    }
+
+    /**
+     * Whether to skip this goal. If skipPrepare is set to false, this parameter has no influence.
+     */
+    @Parameter(property = "skipTests", defaultValue = "false")
+    public void setSkipTests(final boolean skipTests) {
+        this.skipTests = skipTests;
+    }
+
+    /**
+     * If enabled, the goal will not fail the build if the agent artifact is not present.
+     */
+    @Parameter(name = "failSilent", property = "mockito.failSilent", defaultValue = "false")
+    public void setFailSilent(final boolean failSilent) {
+        this.failSilent = failSilent;
+    }
+}
---
+++ src/test/java/dev/jeschke/maven/mockito/PrepareAgentMojoTest.java
@@ -0,0 +1,183 @@
+package dev.jeschke.maven.mockito;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.createTempFile;
+import static java.util.Collections.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.InstanceOfAssertFactories.STRING;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PrepareAgentMojoTest {
+    @TempDir
+    private Path tmpDir;
+    @Mock
+    private MavenProject project;
+    private Properties properties;
+    private PrepareAgentMojo mojo;
+
+    @BeforeEach
+    public void setup() {
+        mojo = new PrepareAgentMojo(project);
+        properties = new Properties();
+        lenient().when(project.getProperties()).thenReturn(properties);
+        lenient().when(project.getBuildPlugins()).thenReturn(emptyList());
+    }
+
+    @Test
+    void execute_defaultValues() throws MojoFailureException, IOException {
+        final Path artifactFile = createTempFile(tmpDir, null, ".jar");
+        when(project.getArtifacts()).thenReturn(singleton(buildArtifact("org.mockito", "mockito-core", artifactFile)));
+
+        mojo.execute();
+
+        assertThat(properties)
+                .extractingByKey("argLine", STRING)
+                .isEqualToIgnoringWhitespace("-javaagent:" + artifactFile.toAbsolutePath());
+    }
+
+    @Test
+    void execute_customValues() throws MojoFailureException, IOException {
+        final Path artifactFile = createTempFile(tmpDir, null, ".jar");
+        when(project.getArtifacts()).thenReturn(singleton(buildArtifact("com.example", "example", artifactFile)));
+        mojo.setPropertyName("customArg");
+        mojo.setAgentGroupId("com.example");
+        mojo.setAgentArtifactId("example");
+
+        mojo.execute();
+
+        assertThat(properties)
+                .doesNotContainKey("argLine")
+                .extractingByKey("customArg", STRING)
+                .isEqualToIgnoringWhitespace("-javaagent:" + artifactFile.toAbsolutePath());
+    }
+
+    @Test
+    void execute_autoDetectTycho() throws MojoFailureException, IOException {
+        final Path artifactFile = createTempFile(tmpDir, null, ".jar");
+        when(project.getArtifacts()).thenReturn(singleton(buildArtifact("org.mockito", "mockito-core", artifactFile)));
+        when(project.getBuildPlugins()).thenReturn(singletonList(buildPlugin("org.eclipse.tycho", "tycho-surefire-plugin")));
+
+        mojo.execute();
+
+        assertThat(properties)
+                .doesNotContainKey("argLine")
+                .extractingByKey("tycho.testArgLine", STRING)
+                .isEqualToIgnoringWhitespace("-javaagent:" + artifactFile.toAbsolutePath());
+    }
+
+    @Test
+    void execute_manualPropertyNameOverridesTycho() throws MojoFailureException, IOException {
+        final Path artifactFile = createTempFile(tmpDir, null, ".jar");
+        when(project.getArtifacts()).thenReturn(singleton(buildArtifact("org.mockito", "mockito-core", artifactFile)));
+        lenient().when(project.getBuildPlugins()).thenReturn(singletonList(buildPlugin("org.eclipse.tycho", "tycho-surefire-plugin")));
+        mojo.setPropertyName("customArg");
+
+        mojo.execute();
+
+        assertThat(properties)
+                .doesNotContainKey("argLine")
+                .extractingByKey("customArg", STRING)
+                .isEqualToIgnoringWhitespace("-javaagent:" + artifactFile.toAbsolutePath());
+    }
+
+    @Test
+    void execute_failsWithoutArtifacts() {
+        when(project.getArtifacts()).thenReturn(emptySet());
+
+        assertThatThrownBy(mojo::execute).isInstanceOf(MojoFailureException.class);
+    }
+
+    @Test
+    void execute_failsSilently() {
+        when(project.getArtifacts()).thenReturn(emptySet());
+        mojo.setFailSilent(true);
+
+        assertThatCode(mojo::execute).doesNotThrowAnyException();
+    }
+
+    @Test
+    void execute_nullVerfication_project() {
+        assertThatThrownBy(() -> new PrepareAgentMojo(null))
+                .isNotNull();
+    }
+
+    @Test
+    void execute_nullVerfication_agentGroupId() {
+        mojo.setAgentGroupId(null);
+        assertThatThrownBy(mojo::execute).isInstanceOf(MojoFailureException.class);
+    }
+
+    @Test
+    void execute_nullVerfication_agentArtifactId() {
+        mojo.setAgentArtifactId(null);
+        assertThatThrownBy(mojo::execute).isInstanceOf(MojoFailureException.class);
+    }
+
+    @MethodSource("skipFlags")
+    @ParameterizedTest(name = "skipPrepare={0} skipTests={1} shouldSkip={2}")
+    void execute_skipFlags(final Boolean skipPrepare, final boolean skipTests, final boolean shouldSkip)
+            throws IOException, MojoFailureException {
+        mojo.setSkipPrepare(skipPrepare);
+        mojo.setSkipTests(skipTests);
+
+        final Path artifactFile = createTempFile(tmpDir, null, ".jar");
+        lenient()
+                .when(project.getArtifacts())
+                .thenReturn(singleton(buildArtifact("org.mockito", "mockito-core", artifactFile)));
+
+        mojo.execute();
+
+        if (shouldSkip) {
+            assertThat(properties).doesNotContainKey("argLine");
+        } else {
+            assertThat(properties).containsKey("argLine");
+        }
+    }
+
+    static Stream<Arguments> skipFlags() {
+        return Stream.of(
+                arguments(false, false, false),
+                arguments(false, true, false),
+                arguments(true, false, true),
+                arguments(true, true, true),
+                arguments(null, false, false),
+                arguments(null, true, true));
+    }
+
+    private Artifact buildArtifact(final String groupId, final String artifactId, final Path artifactFile) {
+        final DefaultArtifact artifact =
+                new DefaultArtifact(groupId, artifactId, "1.0.0", "test", "jar", "jar", new DefaultArtifactHandler());
+        artifact.setFile(artifactFile.toFile());
+        return artifact;
+    }
+
+    private Plugin buildPlugin(final String groupId, final String artifactId) {
+        final Plugin plugin = new Plugin();
+        plugin.setGroupId(groupId);
+        plugin.setArtifactId(artifactId);
+        return plugin;
+    }
+}
